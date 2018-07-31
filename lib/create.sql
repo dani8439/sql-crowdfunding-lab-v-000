c07e4bec6c9a3bb9f8115662549bda61250d9f07
Dani Schuhman <dani8439@aol.com>
2018-07-31 17:33:20 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,10 @@
 CREATE TABLE projects (
-
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER
 );
 
 CREATE TABLE users (
